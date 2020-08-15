6f340bb4f5e650d8935102ef1a71d266bdcadd7e
Daniel Roman <dan.roman21@gmail.com>
2020-08-15 11:05:55 -0400

    Added dice roller game

---
+++ diceroller.py
@@ -0,0 +1,33 @@
+import random
+
+min = 1
+
+max = 6
+
+while True:
+
+    print('Do you want me to roll the dice?')
+
+    answer = input()
+
+    if answer.lower() == 'yes' or answer.lower() == 'y':
+
+      dice_roll = random.randint(min, max)
+
+      print(f'''
+
+The number is ...
+
+{dice_roll}
+
+      ''')
+
+    elif answer.lower() == 'no' or answer.lower() == 'n':
+
+      print('Ok...')
+
+      break
+
+    else:
+
+      print('Give me a real answer...')
