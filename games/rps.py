942f34bc22641852c261b9ffd338640746af3a3d
Daniel Roman <dan.roman21@gmail.com>
2020-08-21 22:00:06 -0400

    Removed commented out sections from exercise

--- games/rps.py
+++ games/rps.py
@@ -1,16 +1,12 @@
 from random import randint
 
-#create a list of play options
 t = ["Rock", "Paper", "Scissors"]
 
-#assign a random play to the computer
 computer = t[randint(0,2)]
 
-#set player to False
 player = False
 
 while player == False:
-#set player to True
     player = input("Rock, Paper, Scissors?")
     if player == computer:
         print("Tie!")
@@ -31,14 +27,11 @@ while player == False:
             print("You win!", player, "cut", computer)
     else:
         print("That's not a valid play. Check your spelling!")
-    #player was set to True, but we want it to be False so the loop continues
     player = False
     computer = t[randint(0,2)]
 
 t = ["Rock", "Paper", "Scissors"]
 
-#assign a random play to the computer
 computer = t[randint(0,2)]
 
-#set player to False
 player = False
--- projects/randomPassword.py
+++ projects/randomPassword.py
@@ -2,13 +2,11 @@ import random
 
 uppercaseLetter=chr(random.randint(65,90))
 
-#A function do shuffle all the characters of a string
 def shuffle(string):
   tempList = list(string)
   random.shuffle(tempList)
   return ''.join(tempList)
 
-#Main program starts here
 uppercaseLetter1=chr(random.randint(65,90))
 uppercaseLetter2=chr(random.randint(65,90))
 lowercaseLetter1=chr(random.randint(97,122))
@@ -18,10 +16,7 @@ digit2=chr(random.randint(0,9))
 punctuationSign1=chr(random.randint(33,38))
 punctuationSign2=chr(random.randint(33,38))
 
-
-#Generate password using all the characters, in random order
 password = uppercaseLetter1 + uppercaseLetter2 + lowercaseLetter1 + lowercaseLetter2 + digit1 + digit2 + punctuationSign1 + punctuationSign2
 password = shuffle(password)
 
-#Ouput
 print(password)
